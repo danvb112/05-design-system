dcb7c48b615b26817c388fb15de417533f406af2
danvb112 <danielbonasser@gmail.com>
2024-11-25 18:27:39 -0300

    Fix Avatar component issues

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -1,19 +1,19 @@
-import { Avatar, AvatarProps } from '@ignite-ui/react';
 import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
 
 export default {
-    title: 'Data Display/Avatar',
-    component: Avatar,
-    args: {
-        src: 'https://github.com/danvb112.png',
-        alt: 'Daniel Bonasser'
-    }
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/danvb112.png',
+    alt: 'Daniel Bonasser',
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
 
 export const WithFallback: StoryObj<AvatarProps> = {
-    args: {
-        src: undefined,
-    }
+  args: {
+    src: undefined,
+  },
 }
